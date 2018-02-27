b63dc0ddc2ba17edcb2cf651dfac46d092b72de3
Stephen Bellchester <stephen.bellchester@auspost.com.au>
2018-02-27 17:16:46 +1100

    Added 2 part 2

--- src/y2017/2.kt
+++ src/y2017/2.kt
@@ -7,4 +7,13 @@ fun main(args: Array<String>) {
             .map { it.map { it.toInt() } }
     val result1 = map.map { it.max()!! - it.min()!! }.sum()
     println("Result 1: $result1")
+
+    val result2 = map.map { list ->
+        list.map { v1 -> list.map { v2 -> Pair(v1, v2) } }
+                .flatten()
+                .filter { it.first != it.second }
+                .first { it.first % it.second == 0 }
+                .run { this.first / this.second }
+    }.sum()
+    println("Result 2: $result2")
 }
